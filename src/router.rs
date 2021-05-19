c5453358ed6cab0a86506f8d19e0aa111ff0d202
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2021-05-19 16:16:05 +0200

    [Router] Properly handle Ack and Nack

--- src/router.rs
+++ src/router.rs
@@ -18,12 +18,22 @@ pub trait Router {
     fn default_routes(&mut self, command: Command) -> Option<Command> {
         match command.command() {
             PING => self.handle_ping(command),
+            ACK => self.handle_ack(command),
+            NACK => self.handle_nack(command),
             _ => self.default_handler(command),
         }
     }
 
     fn handle_ping(&mut self, _command: Command) -> Option<Command> {
         Some(Command::ack())
+    }
+
+    fn handle_ack(&mut self, _command: Command) -> Option<Command> {
+        None
+    }
+
+    fn handle_nack(&mut self, _command: Command) -> Option<Command> {
+        None
     }
 
     fn default_handler(&mut self, _command: Command) -> Option<Command> {
@@ -54,13 +64,32 @@ mod tests {
         );
     }
 
+    #[test]
+    fn an_ack_replies_nothing() {
+        assert_eq!(DefaultRouter.route(Command::ack(), &mut ()), None);
+    }
+
+    proptest! {
+        #[test]
+        fn a_nack_replies_nothing(
+            reason in vec(0..=u8::MAX, 0..=u8::MAX as usize)
+        ) {
+            let nack = Command::new(NACK, &reason).unwrap();
+            assert_eq!(DefaultRouter.route(nack, &mut ()), None);
+        }
+    }
+
     proptest! {
         #[test]
         fn any_other_command_replies_a_nack_unknown_command(
             command in 0..=u8::MAX,
             value in vec(0..=u8::MAX, 0..=u8::MAX as usize),
         ) {
-            prop_assume!(command != PING);
+            prop_assume!(
+                command != PING
+                && command != ACK
+                && command != NACK
+            );
 
             let command = Command::new(command, &value).unwrap();
             let nack =
