da21d6ffd89fb7fad4832749c8842685ae5f58d2
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2021-05-24 14:24:45 +0200

    [version] Add a module to store the protocol & library versions

---
+++ src/version.rs
@@ -0,0 +1,17 @@
+//! ERCP version.
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Version {
+    pub major: u8,
+    pub minor: u8,
+    pub patch: u8,
+}
+
+pub const PROTOCOL_VERSION: Version = Version {
+    major: 0,
+    minor: 1,
+    patch: 0,
+};
+
+pub const LIBRARY_VERSION: &str =
+    concat!("ercp_basic.rs ", env!("CARGO_PKG_VERSION"));
