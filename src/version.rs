5a85ff2b6613bfd34c42e21b92b7982047e7bf80
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2021-11-18 20:05:28 +0100

    docs(version): document the module

--- src/version.rs
+++ src/version.rs
@@ -1,17 +1,23 @@
-//! ERCP version.
+//! ERCP Basic protocol and library versions.
 
+/// A protocol version.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Version {
+    /// The major version, denoting API-incompatible changes.
     pub major: u8,
+    /// The minor version, denoting new features.
     pub minor: u8,
+    /// The patch version, denoting fixes.
     pub patch: u8,
 }
 
+/// The version of ERCP Basic implemented by the library.
 pub const PROTOCOL_VERSION: Version = Version {
     major: 0,
     minor: 1,
     patch: 0,
 };
 
+/// The version of `ercp_basic.rs`.
 pub const LIBRARY_VERSION: &str =
     concat!(env!("CARGO_PKG_NAME"), ".rs ", env!("CARGO_PKG_VERSION"));
