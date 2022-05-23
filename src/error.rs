f5f716971ae4ac815cd51030bd8855091468f4bf
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-05-23 23:37:22 +0200

    feat(error): implement Display for ReceivedCommandError

--- src/error.rs
+++ src/error.rs
@@ -227,6 +227,34 @@ impl<IoError> std::fmt::Display for CommandError<IoError> {
 }
 
 #[cfg(feature = "std")]
+impl<IoError> std::fmt::Display for ReceivedCommandError<IoError> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::IoError(_) => write!(f, "IO error"),
+            Self::ReceivedFrameError(ReceivedFrameError::UnexpectedValue) => {
+                write!(f, "an unexpected start sequence has been received")
+            }
+            Self::ReceivedFrameError(ReceivedFrameError::TooLong) => {
+                write!(f, "the received frame is too long")
+            }
+            Self::ReceivedFrameError(ReceivedFrameError::NotEot) => {
+                write!(f, "the received frame is improperly terminated")
+            }
+            Self::ReceivedFrameError(ReceivedFrameError::InvalidCrc) => {
+                write!(f, "the received frame has an invalid CRC")
+            }
+            Self::ReceivedFrameError(ReceivedFrameError::Overflow) => {
+                write!(
+                    f,
+                    "some data has been received while the previous command is still being processed"
+                )
+            }
+            Self::Timeout => write!(f, "timeout"),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
 impl std::fmt::Display for GenericCommandError {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
